683f4adf953b488bba923a86d40ea6f865a956b3
Srinivas Akella <srinivas.prasad@teknotrait.com>
2023-03-22 12:40:43 +0530

    first commti

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Hello world</div>
+    </>
   );
 }
 
 export default App;
+
+// cloudinary url:
+// ("upload_preset", "myBook");
+// ("cloud_name", "dakda5ni3");
+// fetch("https://api.cloudinary.com/v1_1/dakda5ni3/image/upload", {
+//   method: "post",
+//   body: data,
+// })
